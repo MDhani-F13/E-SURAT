cb567f900a2259548da0874bfae90aef321a68a2
MDhani-F13 <cellarj17@gmail.com>
2019-11-04 14:12:00 +0700

    first commit

---
+++ views/site/index.php
@@ -0,0 +1,17 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'My Yii Application';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>E-SURAT</h1>
+
+        <p class="lead">Selamat datang di E-SURAT</p>
+
+        <p><a class="btn btn-lg btn-success" href="http://localhost/basic/web/index.php?r=surat-masuk">Masuk Ke Form</a></p>
+    </div>
+    </div>
+</div>
